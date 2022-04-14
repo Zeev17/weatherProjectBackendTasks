77b893ed3460b221440490686d81fe1996e3d660
zeev <zeev@zeev.com>
2022-04-14 16:49:57 +0200

    Change id from String to Long

--- backendTasks/src/main/java/com/weatherProject/backendTasks/entity/Temperature.java
+++ backendTasks/src/main/java/com/weatherProject/backendTasks/entity/Temperature.java
@@ -4,17 +4,17 @@ import java.util.Date;
 
 public class Temperature {
 
-    private  String id;
+    private  Long id;
     private  short kelvin;
     private  String nameCity ;
     private String idCity;
     private Date recordCreated;
 
-    public String getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(String id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
