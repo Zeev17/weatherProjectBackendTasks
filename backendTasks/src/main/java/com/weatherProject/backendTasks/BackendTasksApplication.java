a7f4f9af787edcc5b05a312530a90b9cbdae48b7
Antoine PAPINI <30653676+Zeev17@users.noreply.github.com>
2022-04-14 15:45:07 +0200

    initial commit

---
+++ backendTasks/src/main/java/com/weatherProject/backendTasks/BackendTasksApplication.java
@@ -0,0 +1,13 @@
+package com.weatherProject.backendTasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendTasksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendTasksApplication.class, args);
+	}
+
+}
