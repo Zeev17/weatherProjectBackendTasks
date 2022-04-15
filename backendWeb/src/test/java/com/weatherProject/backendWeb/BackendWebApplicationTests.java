fd9ab961d9de023aa1afe30944fc6d34988c0f4b
zeev <zeev@zeev.com>
2022-04-15 13:35:06 +0200

    Add a new module BackenWeb

---
+++ backendWeb/src/test/java/com/weatherProject/backendWeb/BackendWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weatherProject.backendWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
